457b9700e7543c7b6302a97a74580c74b9f2151c
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-11-08 08:51:01 -0600

    Plantilla
    Plantilla dashboard

--- Gestor/routes/web.php
+++ Gestor/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login',[AdminController::class, 'login']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
